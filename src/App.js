425cf18334049ebb82ff861ddf2d1f5303d47864
Endersson Alonso Mendoza Muñoz <endermendoza12@gmail.com>
2023-03-17 18:17:22 -0600

    Agregados primeros componentes y estructura de carpetas.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from "./components/home/header/Header";
+import { BrowserRouter as Router, Switch, Routes, Route, Link } from "react-router-dom";
+import HomePages from './components/pagecontent/HomePages';
 
 function App() {
   return (
     <div className="App">
-
+      <Router>
+        <Header />
+        <Routes>
+          <Route path='/' exact Component={HomePages} />
+        </Routes>
+      </Router>
     </div>
   );
 }
