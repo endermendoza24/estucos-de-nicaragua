d9785b92d94a02161b892d103bda6111ab251dbd
Endersson Alonso Mendoza Muñoz <endermendoza12@gmail.com>
2023-03-19 17:47:27 -0600

    Terminado sitio, aún hay que mejorar varias cosas

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 import Header from "./components/home/header/Header";
-import { BrowserRouter as Router, Switch, Routes, Route, Link } from "react-router-dom";
-import HomePages from './components/pagecontent/HomePages';
+import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
+import HomePages from "./components/pagecontent/HomePages";
+
 
 function App() {
-  return (
-    <div className="App">
-      <Router>
-        <Header />
-        <Routes>
-          <Route path='/' exact Component={HomePages} />
-        </Routes>
-      </Router>
-    </div>
-  );
+	return (
+		<div className="App">
+			<Router>
+				<Header/>
+				<Routes>
+					<Route path="/" exact Component={HomePages}/>
+				</Routes>
+			</Router>
+		</div>
+	);
 }
 
 export default App;
